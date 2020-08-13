e3344b98d33e14f4ca4765cb30ca7a15dfab1fce
Andie Vester <avester146@gmail.com>
2020-08-13 14:30:21 -0400

    submitting potluckdetail

--- frontend/src/views/PotluckDetail.vue
+++ frontend/src/views/PotluckDetail.vue
@@ -24,27 +24,41 @@
     </div>
     <div class="detail-box">
       <div class="detail-container">
-        <div class="detail-items">
-          <div class="potluck-details">
-            <h1>{{ displayedPotluck.name }}</h1>
-            <h2>Location: {{ displayedPotluck.location }}</h2>
-            <h2>Date: {{displayedPotluck.potluck_date }}</h2>
-            <h2>Time: {{ displayedPotluck.potluck_time }}</h2>
-            <h2>Decription: {{displayedPotluck.description }}</h2>
-            <h2>Guests({{ displayedPotluck.guests }}):</h2>
-            <h2>Appetizers({{ displayedPotluck.appetizers }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Appetizer" />
-            <h2>Entrees({{ displayedPotluck.entrees }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Entree" />
-            <h2>Side Dishes({{ displayedPotluck.side_dishes }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Side Dish" />
-            <h2>Desserts({{ displayedPotluck.desserts }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Dessert" />
-            <h2>Beverages({{ displayedPotluck.non_alcohol }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Non-Alcoholic Beverage" />
-            <h2>Alcohol({{ displayedPotluck.alcohol }}):</h2>
-            <dish-list v-bind:allDishes="dishDetails" selectedCategory="Alcoholic Beverage" />
+        <div class="details-container-1">
+          <div class="item">
+            <h2 class="category-1">{{ displayedPotluck.name }}</h2>
           </div>
+          <div class="item">
+            <h3 class="category">Location:</h3>
+            <h4 id="item">{{ displayedPotluck.location }}</h4>
+          </div>
+          <div class="item">
+            <h3 class="category">Date:</h3>
+            <h4 id="item">{{displayedPotluck.potluck_date }}</h4>
+          </div>
+          <div class="item">
+            <h3 class="category">Time:</h3>
+            <h4 id="item">{{ displayedPotluck.potluck_time }}</h4>
+          </div>
+          <div class="item">
+            <h3 class="category">Description:</h3>
+            <h4 id="item">{{displayedPotluck.description }}</h4>
+          </div>
+        </div>
+        <div class="details-container-2">
+          <h3>Guests({{ displayedPotluck.guests }}):</h3>
+          <h3>Appetizers({{ displayedPotluck.appetizers }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Appetizer" />
+          <h3>Entrees({{ displayedPotluck.entrees }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Entree" />
+          <h3>Side Dishes({{ displayedPotluck.side_dishes }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Side Dish" />
+          <h3>Desserts({{ displayedPotluck.desserts }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Dessert" />
+          <h3>Beverages({{ displayedPotluck.non_alcohol }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Non-Alcoholic Beverage" />
+          <h3>Alcohol({{ displayedPotluck.alcohol }}):</h3>
+          <dish-list v-bind:allDishes="dishDetails" selectedCategory="Alcoholic Beverage" />
         </div>
       </div>
     </div>
@@ -90,13 +104,11 @@ export default {
         non_alcohol: "",
         alcohol: "",
       },
-      dishDetails: []
-      
-    }
+      dishDetails: [],
+    };
   },
   components: {
-    
-    DishList
+    DishList,
   },
   created() {
     PotluckService.getPotluck(this.$route.params.id)
@@ -108,9 +120,10 @@ export default {
           this.$router.push("/not-found");
         }
       });
-        PotluckService.getDish(this.$route.params.id).then((response) => (this.dishDetails = response.data));
+    PotluckService.getDish(this.$route.params.id).then(
+      (response) => (this.dishDetails = response.data)
+    );
   },
-
 };
 </script>
 <style>
@@ -123,10 +136,11 @@ export default {
 
 .detail-container {
   display: flex;
-  justify-content: space-between;
+  flex-direction: column;
+  /* justify-content: space-evenly; */
   background-color: rgba(245, 245, 245, 0.575);
   padding-top: 5px;
-  width: 90%;
+  width: 80%;
   height: 940px;
   border-radius: 1.5em;
   box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
@@ -145,6 +159,54 @@ export default {
   margin-left: 25px;
   margin-top: -75px;
 }
+.item {
+  display: flex;
+  flex-direction: row;
+  margin-bottom: -25px;
+}
+.category-1 {
+  color: black;
+  margin-right: 5px;
+  text-shadow: 2px 4px 4px rgba(0, 0, 0, 0.2),
+    0px -5px 10px rgba(255, 255, 255, 0.15);
+}
+.category {
+  color: dimgray;
+  margin-right: 5px;
+}
+.details-container-1 {
+  display: flex;
+  justify-content: flex-start;
+  flex-direction: column;
+  text-align: left;
+  margin-left: 35px;
+  background-color: rgba(245, 245, 245, 0.541);
+  border-radius: 1.5em;
+  box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+  width: 90%;
+  margin-right: 25px;
+  margin-top: 15px;
+  padding-left: 15px;
+  padding-right: 15px;
+  padding-bottom: 25px;
+  margin-bottom: 50px;
+}
+.details-container-2 {
+  display: flex;
+  justify-content: flex-end;
+  flex-direction: column;
+  text-align: left;
+  padding-top: 5px;
+  background-color: rgba(105, 105, 105, 0.425);
+  border-radius: 1.5em;
+  box-shadow: 0px 11px 35px 2px rgba(0, 0, 0, 0.14);
+  width: 90%;
+  margin-left: 35px;
+  margin-right: 25px;
+  padding-left: 15px;
+  padding-right: 15px;
+}
+
 .button-container {
   display: flex;
   flex-direction: row;
@@ -163,17 +225,17 @@ export default {
 }
 
 #bring {
-font-style: oblique;
-padding-left: 15px;
-padding-bottom: 10px;
-padding-top: 10px;
-font-size: 20px;
+  font-style: oblique;
+  padding-left: 15px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  font-size: 20px;
 }
 #invite {
-font-style: oblique;
-padding-right: 15px;
-padding-bottom: 10px;
-padding-top: 10px;
-font-size: 20px;
+  font-style: oblique;
+  padding-right: 15px;
+  padding-bottom: 10px;
+  padding-top: 10px;
+  font-size: 20px;
 }
 </style>
